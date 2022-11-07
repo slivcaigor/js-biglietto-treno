0ad682e1af3f7571fe790e9aa9f7f3437a82d2fe
slivcaigor <slivcaigor@gmail.com>
2022-11-07 13:20:05 +0100

    Layout

---
+++ js/master.js
@@ -0,0 +1,14 @@
+//Calcolo biglietto treno
+
+
+//Chiedere numero di KM da percorrere
+
+//Chiedere l'eta' del passeggero
+
+//Il prezzo e' definito in base ai KM (0.21 euro al KM)
+
+//Applicare sconto del 20% se minorenni
+
+//Applicare sconto del 40% se over 65
+
+//Output con massimo due decimali
